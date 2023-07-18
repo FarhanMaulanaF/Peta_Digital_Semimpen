139aa301e05a4ce0eb98d1fe442850b3a6502516
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-07-18 23:29:30 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     '././**/*.js',
 ],
   theme: {
+    spacing: {
+      some_key: {
+        1.5: '1.5rem',
+      },
+    },
     colors: {
       'primary': '#9333ea',
     },
