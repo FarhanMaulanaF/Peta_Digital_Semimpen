cd0c5219d7631d2cf6134c2665146eb09960e2f2
FarhanMaulanaF <farhanmaulanafirmansyah@mail.ugm.ac.id>
2023-07-25 11:32:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function success(position) {
     });
 
     // Add marker and circle
-    marker = L.marker([latitude, longitude], { icon: userIcon }).addTo(map).bindPopup("<p class='text-xl text-[#f00] font-bold'>Lokasi anda sekarang. Coba mengetesss</p>");
+    marker = L.marker([latitude, longitude], { icon: userIcon }).addTo(map).bindPopup("<p class='text-xl text-[#f00] font-bold'>Lokasi anda sekarang</p>");
     circle = L.circle([latitude, longitude], { radius: accuracy }).addTo(map);
 
     // Zoom map to fit marker and circle
